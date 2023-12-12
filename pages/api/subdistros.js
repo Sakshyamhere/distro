477ea091ba5a38a364501004b75a834962340c67
Sakshyamhere <aryalsakshyam33@gmail.com>
2023-12-12 23:36:49 +0545

    files

--- pages/api/subdistros.js
+++ pages/api/subdistros.js
@@ -1,16 +1,16 @@
 import fs from "fs";
 
 export default async function subDistroData(req, res) {
-    try {
-        const filePath = `./subdistro/${req.query.slug}.json`;
+  try {
+    const filePath = `./subdistro/${req.query.slug}.json`;
 
-        const data = await fs.promises.readFile(filePath, "utf8");
-        console.log(data)
-        // Sending the file content as the response
+    const data = await fs.promises.readFile(filePath, "utf8");
 
-        res.status(200).send(data);
-    } catch (err) {
-        // Error occurred while reading or file not found
-        res.status(500).send("Not Found");
-    }
+    // Sending the file content as the response
+
+    res.status(200).send(data);
+  } catch (err) {
+    // Error occurred while reading or file not found
+    res.status(500).send("Not Found");
+  }
 }
